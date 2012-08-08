f50b7baabfc42d8147021631adf7ce14a79174b1
justinalexander <justin@socialatitude.com>
2012-08-08 10:53:23 -0400

    finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,13 @@ SampleApp::Application.routes.draw do
   get "pages/contact"
   
   get "pages/about"
+  
+  #match "/pages/home", :to => redirect("/")
+ 
+  
 
+#match '/', to: 'pages#home'
+#route "pages#home", to: '/'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -54,7 +60,7 @@ SampleApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+   root :to => "pages#home"
 
   # See how all your routes lay out with "rake routes"
 
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -11,9 +11,14 @@ describe PagesController do
     end
     
    it "should have the right title" do
-      get 'home'
-        page.should have_selector('title', :content => "Home")
+      visit 'home'
+         page.should have_selector('title', :content => "Home")
    end
+   
+   it "should have a non-blank body" do
+     visit 'home'
+      response.body.should_not =~ /<body>\s*<\/body>/
+    end
   end
 
   describe "GET 'contact'" do
@@ -21,12 +26,22 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+     
+     it "should have the right title" do
+       visit 'contact'
+           page.should have_selector('title', :content => "Contact")
+    end
   end
 
   describe "GET 'about'" do
     it "returns http success" do
       get 'about'
       response.should be_success
+    end
+     
+     it "should have the right title" do
+       visit 'about'
+          page.should have_selector('title', :content => "About")
     end
   end
   
